4a5fde0628d87993fa7e9696043f0faf9cdfaef5
Will DeBerry <willdeberry@gmail.com>
2021-06-12 19:46:50 -0400

    Initial commit

---
+++ Sudo Parent/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sudo Parent
+//
+//  Created by Will DeBerry on 6/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sudo Parent/Sudo_ParentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sudo_ParentApp.swift
+//  Sudo Parent
+//
+//  Created by Will DeBerry on 6/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Sudo_ParentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
